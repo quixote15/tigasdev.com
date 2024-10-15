744e4dc48a827ca3078eb785c54413f7fada598e
quixote15 <santos2tiago@gmail.com>
2024-10-15 07:30:25 -0300

    feat: add ayo section and arrows

---
+++ src/components/ ExperienceGaleryEntry.js
@@ -0,0 +1,63 @@
+import clsx from "clsx"
+
+export const PictureRotatorItem = ({photo}) => {
+  return (
+    <div>
+        <div className={clsx('flex flex-col px-1 pb-2 bg-white rounded-md -rotate-12', photo.className, photo.disableRotate && 'rotate-0')} >
+            <img src={photo.url} className={clsx('mt-1', photo.height ?? 'h-36', photo.width ?? 'w-10/11' )}/>
+            <label className='text-dark font-nanum text-xl text-center'>{photo.label}</label>
+        </div>
+        {photo.arrowComponent?.()}
+    </div>
+  )
+}
+export const PicturesRotator = ({photos}) => {
+  if(photos.length === 1)  {
+    const [photo] = photos
+    return (
+     <PictureRotatorItem photo={photo} />
+    )
+  }
+
+  return photos.map((photo, index) => (
+      <div key={photo.url} className={clsx('flex flex-col px-1 pb-2  rounded-md mt-3', index % 2 === 0 ? '-rotate-12' : 'rotate-12', `z-${index * 10}`, photo.className || 'bg-white',  photo.disableRotate && 'rotate-0')} >
+          <img src={photo.url} className={clsx('', photo.height ?? 'h-36', photo.width ?? 'w-10/11' )}/>
+          <label className='text-dark font-nanum text-xl text-center'>{photo.label}</label>
+          {console.log(photo)}
+      </div>
+  ))
+}
+
+export const ExperienceGaleryEntry = ({experience, position}) => {
+
+ 
+    return (
+        <div className='flex flex-col justify-between mt-2'>
+        <div className={clsx("flex items-center justify-around", position === 'right' ? 'flex-row-reverse' : 'flex-row')}>
+          <div className='flex flex-col '>
+             <PicturesRotator photos={experience.photos} />
+          </div>
+      
+          <div className='flex flex-col w-1/2 ml-6'>
+            <h1 className='text-xl text-blue-300 text-center'>{experience.title}</h1>
+            <label className='text-md text-blue-300 mb-4 text-center'>{experience.period}</label>
+              {experience.paragraphs.map((paragraph) => (
+                    <label className="text-white text-sm text-center  mb-2 font-bold"> {paragraph}</label>
+              ) )}
+            <div className="flex flex-row gap-2 ">
+              {experience.achievements.map((achievement) => (
+                <div key={achievement} className="bg-slate-900 p-2 rounded-md w-26">
+                  <label className="text-white text-xs text-center  text-ellipsis">
+                    {achievement}
+                  </label>
+                </div>
+              ))}
+            </div>
+
+          </div>
+
+         
+        </div>
+      </div>
+    )
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
+import { ExperienceEntry } from "@components/ ExperienceEntry"
 
+import { ExperienceRowEntry } from "@components/ ExperienceRowEntry"
+import { ExperienceGaleryEntry } from "@components/ ExperienceGaleryEntry"
 
 const skills = [
   "Backend (NodeJS)",
@@ -11,6 +14,47 @@ const skills = [
   "Message Brokers (SNS, SQS, Kafka)",
   "Design System",
   "Microservices design patterns"
+]
+
+const experiences = [
+  {
+    title: "Software Engineer",
+    period: "2021 - Present",
+    paragraphs: [],
+    achievements: [],
+    photos: []
+  },
+
+  {
+    title: "Softeam",
+    period: "2017 - 2019",
+    paragraphs: ['While I was finishing my studies,  I volunteered Softeam to make a difference and learn something in the process. Softeam is a junior enterprise, which basically is a civil social nonprofit organization established and executed entirely by students of a university.', 'I was lucky to work with great people who were smart, friendly, coffee lovers and always willing to develop projects and help me whenever needed. I Thank you all for having my back :)'],
+    achievements: ['The marshmallow challenge is a classic problem-solving and team building exercise. I was happy to have won this challenge with my team on an PMI event.', 'I joined softeam as a frontend developer and finished as a project manager of a small team of devs. During my time there I was able to deliver projects on time and that helped the company achieve high revenue growth for 3 consecutive years.'],
+    photos: [{ url: '/images/marshmallow-challenge.png', label: 'The Marshmallow - 2019' }, { url: '/images/sermej.png', label: 'High growth celebration - 2019' }]
+  },
+
+  {
+    title: "Fecho",
+    period: "2019 - 2021",
+    paragraphs: ['I co-founded a startup with peers to revolutionize the Brazilian real estate market by integrating Artificial Intelligence as a mediator between buyers, sellers, and property developers.', ' Fun fact: Fecho, our product, originated from my undergraduate thesis and represents the pinnacle of my academic journey. It allowed me to consolidate the knowledge and skills I gained throughout college, and has shaped my development as a software engineer and entrepreneur, preparing me for the challenges ahead.'],
+    achievements: ['There was no AI Hype nor ChatGPT API/models at the time so I had to go through all the papers on machine learning and Natural language processing in order to build a human-like buying experience that scales well.', 'Integration was the key to our success. I managed to develop an omnichannel platform that integrated with WhatsApp, Slack, Web, and Mobile, allowing us to reach a broader audience and reach our client no matter where they were.'],
+    photos: [{ url: '/images/fechoapp.jpeg', label: 'The Marshmallow - 2019', className: 'bg-transparent', width: 'w-30', height: 'h-60 rounded-md', disableRotate: true }, {url: '/images/node-t-shirt.png'}]
+  },
+   {
+    title: "Fecho",
+    period: "2019 - 2021",
+    paragraphs: ['I co-founded a startup with peers to revolutionize the Brazilian real estate market by integrating Artificial Intelligence as a mediator between buyers, sellers, and property developers.'],
+    achievements: [],
+    photos: [{ url: '/images/fechoapp.png', label: '', className: 'bg-transparent', width: 'w-30', height: 'h-60 rounded-md', disableRotate: true }, {url: '/images/t-shirt.png'}]
+  },
+
+   {
+    title: "Ayo",
+    period: "2019 - 2021",
+    paragraphs: ['I left my comfort zone and saw an opportunity to start my own project. It was a really good time.', 'It was a time of constant experimentation and a lot of work in different areas of design.', 'I learned a lot about software development, marketing, and self-management. In collaboration with talented people we designed beautiful projects.'],
+    achievements: [],
+    photos: [ {url:'/images/me-at-ayo.jpeg', label: 'AyoApp HQ - 2021', className: 'bg-white', width: 'object-cover', disableRotate: true},{ url: '/images/ayo.png', label: '', className: 'bg-transparent', width: 'w-30 object-scale-down', height: 'h-60 rounded-md', disableRotate: true }, {url: '/images/ayopay.png', width: 'w-10 object-fill' , height: 'h-10 rounded-full', className: 'bg-transparent', disableRotate: true}]
+  },
 ]
 
 export default function Home() {
@@ -70,40 +114,38 @@ export default function Home() {
               </label>
             </div>
 
-            <div className='flex flex-col justify-between mt-2'>
-              <div className="flex flex-row items-center justify-around">
-                <div className='flex flex-col '>
-                  <div className='flex flex-col px-1 pb-2 bg-white rounded-md -rotate-12' >
-                    <img src='/images/graduation-cerimony.JPG' className='w-48 h-36 mt-1' />
-                    <label className='text-dark font-nanum text-xl text-center'>Sergipe BR - 2019</label>
-                  </div>
-                  <img src='/images/arrow-up-left.svg' className='w-20 h-30 z-10 self-center relative -left-10 -top-14' />
-                  <label className="bg-gradient-to-r from-emerald-200 to-blue-500 inline-block text-transparent bg-clip-text font-bold font-mono text-lg text-right relative -top-20 mt-2 mr-3">That's Me!</label>
-                </div>
-                <div className='flex flex-col w-1/2 ml-6'>
-                  <h1 className='text-xl text-blue-300  '>School</h1>
-                  <label className='text-md text-blue-300 mb-4 '>2014 - 2019</label>
-                  <label className="text-white text-md  mb-2 font-bold">
-                    I started studying at the Universidade Federal de Sergipe in Brazil
-                  </label>
-                  <label className="text-md text-white font-bold">
-                    In my early years, I explored competitive programming, web dev, AI entrepreneurship and also met great people.
-                  </label>
-                  <div className="flex flex-row gap-2 ">
-                    <div className=" bg-slate-900 p-2 rounded-md mt-4 w-26">
-                      <label className=" max-w-20 text-white text-xs text-center  text-ellipsis">
-                        I was very good at the programming contests and event competed in the first two years
-                      </label>
-                    </div>
-                    <div className="w-26 bg-slate-900 p-2 rounded-md mt-4 text-ellipsis" >
-                      <label className="max-w-20 text-white text-xs text-center ">
-                      I found the thrill of using technology to create new projects and products incredibly inspiring. This passion often kept me awake through countless nights during hackathons.
-                      </label>
-                    </div>
-                  </div>
-                </div>
-              </div>
+            <ExperienceEntry experience={
+              {
+                title: "School",
+                period: "2024 - 2019",
+                paragraphs: ['I started studying at the Universidade Federal de Sergipe in Brazil', 'In my early years, I explored competitive programming, web dev, AI entrepreneurship and also met great people.'],
+                achievements: ['I was very good at the programming contests and event competed in the first two years', 'I found the thrill of using technology to create new projects and products incredibly inspiring. This passion often kept me awake through countless nights during hackathons.'],
+                photos: [{
+                  url: '/images/graduation-cerimony.JPG', label: 'Sergipe BR - 2019', arrowComponent: () => (<div className="flex flex-col">
+                    <img src='/images/arrow-up-left.svg' className='w-20 h-30 z-10 self-center relative -left-10 -top-14' />
+                    <label className="bg-gradient-to-r from-emerald-200 to-blue-500 inline-block text-transparent bg-clip-text font-bold font-mono text-lg text-right relative -top-20 mt-2 mr-3">That's Me!</label>
+                  </div>)
+                }]
+              }
+            } position={'left'} />
+
+            <div className="flex flex-row-reverse ">
+              <img src="/images/arrow-down-round.svg" className="h-2/3 w-1/3 self-center relative top-0" />
             </div>
+
+            <ExperienceEntry experience={experiences[1]} position={'left'} />
+
+            <div className="flex flex-row-reverse">
+              <img src="/images/arrow-down-orange.svg" className="h-2/3 w-1/3 mr-32 " />
+            </div>
+            <ExperienceRowEntry experience={experiences[3]} position={'left'} />
+
+            <div className="flex flex-row-reverse justify-center">
+              <img src="/images/arrow-down-center.svg" className="h-1/3 w-1/5" />
+            </div>
+
+
+            <ExperienceGaleryEntry experience={experiences[4]} position={'left'} />
 
           </div>
         </div>
